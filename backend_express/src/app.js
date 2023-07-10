c492031a5f99f1b2213873fd2a97f3e1e97bd68d
Leonardo Ramírez <31749560+LeoRami99@users.noreply.github.com>
2023-07-10 16:42:40 -0500

    Se añade acces logs y errores de servidor

--- backend_express/src/app.js
+++ backend_express/src/app.js
@@ -10,6 +10,7 @@ const app = express();
 
 // Crear un stream de escritura en modo 'append'
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+var errorLogStream = fs.createWriteStream(path.join(__dirname, 'error.log'), { flags: 'a' })
 
 // Configurar morgan para usar el stream de escritura
 app.use(morgan('combined', { stream: accessLogStream }))
@@ -20,6 +21,16 @@ app.use(express.json());
 app.use('/curso', cursoRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/imagenes', express.static(path.join(__dirname, 'images/curso')));
+
+// Middleware de manejo de errores
+app.use(function (err, req, res, next) {
+  // Loguear el error en nuestro archivo de log de errores
+  const errorLog = `${new Date().toISOString()} - Error: ${err.message}\nStack: ${err.stack}\n\n`;
+  errorLogStream.write(errorLog);
+
+  // Enviar respuesta al cliente
+  res.status(500).send('Ocurrió un error en el servidor');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
