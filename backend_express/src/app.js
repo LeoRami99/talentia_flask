396cbc2a23f62b158cac8f14ef810c9718177fc7
Leonardo Ramírez <31749560+LeoRami99@users.noreply.github.com>
2023-07-10 16:38:31 -0500

    Se añade morgan al servidor para generar logs

--- backend_express/src/app.js
+++ backend_express/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
+const fs = require('fs');
 const cursoRoutes = require('./api/cursos/cursoRoutes');
 const usuarioRoutes = require('./api/usuarios/usuarioRoutes');
 
 const app = express();
+
+// Crear un stream de escritura en modo 'append'
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+// Configurar morgan para usar el stream de escritura
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +20,6 @@ app.use(express.json());
 app.use('/curso', cursoRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/imagenes', express.static(path.join(__dirname, 'images/curso')));
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
